8249cd44652b6922f832fcab0e510e3ed6cc9f43
Joseph Tubridy and Trevor Teigen <trevor@>
2019-06-06 13:40:54 -0700

    add pictures and finished js logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,81 +2,92 @@ $(function(){
 var gender;
 var color;
 
+//Hides all contestants
+    $("#piggy").addClass("hiding");
+    $("#gladys").addClass("hiding");
+    $("#spamela").addClass("hiding");
+    $("#wanda").addClass("hiding");
+    $("#animal").addClass("hiding");
+    $("#beaker").addClass("hiding");
+    $("#scooter").addClass("hiding");
+    $("#kermit").addClass("hiding");
+
   $(".formOne").submit(function(event){
     color = $("#color").val();
     gender =$("#gender").val();
+//Reveals contestants based on user preferences
       if (gender == 1 && color == "grn") {
-      $("#piggy").hide();
-      $("#gladys").hide();
-      $("#spamela").hide();
-      $("#wanda").hide();
-      $("#animal").hide();
-      $("#beaker").hide();
-      $("#scooter").hide();
-      $("#kermit").show();
+      $("#piggy").addClass("hiding");
+      $("#gladys").addClass("hiding");
+      $("#spamela").addClass("hiding");
+      $("#wanda").addClass("hiding");
+      $("#animal").addClass("hiding");
+      $("#beaker").addClass("hiding");
+      $("#scooter").addClass("hiding");
+      $("#kermit").removeClass("hiding");
     } else if (gender == 1 && color == "yel") {
-      $("#piggy").hide();
-      $("#gladys").hide();
-      $("#spamela").hide();
-      $("#wanda").hide();
-      $("#animal").hide();
-      $("#beaker").hide();
-      $("#scooter").show();
-      $("#kermit").hide();
+      $("#piggy").addClass("hiding");
+      $("#gladys").addClass("hiding");
+      $("#spamela").addClass("hiding");
+      $("#wanda").addClass("hiding");
+      $("#animal").addClass("hiding");
+      $("#beaker").addClass("hiding");
+      $("#scooter").removeClass("hiding");
+      $("#kermit").addClass("hiding");
     } else if (gender == 1 && color == "pink") {
-      $("#piggy").hide();
-      $("#gladys").hide();
-      $("#spamela").hide();
-      $("#wanda").hide();
-      $("#animal").hide();
-      $("#beaker").show();
-      $("#scooter").hide();
-      $("#kermit").hide();
+      $("#piggy").addClass("hiding");
+      $("#gladys").addClass("hiding");
+      $("#spamela").addClass("hiding");
+      $("#wanda").addClass("hiding");
+      $("#animal").addClass("hiding");
+      $("#beaker").removeClass("hiding");
+      $("#scooter").addClass("hiding");
+      $("#kermit").addClass("hiding");
     } else if (gender == 1 && color == "red") {
-      $("#piggy").hide();
-      $("#gladys").hide();
-      $("#spamela").hide();
-      $("#wanda").hide();
-      $("#animal").show();
-      $("#beaker").hide();
-      $("#scooter").hide();
-      $("#kermit").hide();
+      $("#piggy").addClass("hiding");
+      $("#gladys").addClass("hiding");
+      $("#spamela").addClass("hiding");
+      $("#wanda").addClass("hiding");
+      $("#animal").removeClass("hiding");
+      $("#beaker").addClass("hiding");
+      $("#scooter").addClass("hiding");
+      $("#kermit").addClass("hiding");
     } else if (gender == 2 && color == "grn") {
-    $("#piggy").hide();
-    $("#gladys").hide();
-    $("#spamela").hide();
-    $("#wanda").show();
-    $("#animal").hide();
-    $("#beaker").hide();
-    $("#scooter").hide();
-    $("#kermit").hide();
+    $("#piggy").addClass("hiding");
+    $("#gladys").addClass("hiding");
+    $("#spamela").addClass("hiding");
+    $("#wanda").removeClass("hiding");
+    $("#animal").addClass("hiding");
+    $("#beaker").addClass("hiding");
+    $("#scooter").addClass("hiding");
+    $("#kermit").addClass("hiding");
   } else if (gender == 2 && color == "yel") {
-    $("#piggy").hide();
-    $("#gladys").show();
-    $("#spamela").hide();
-    $("#wanda").hide();
-    $("#animal").hide();
-    $("#beaker").hide();
-    $("#scooter").hide();
-    $("#kermit").hide();
+    $("#piggy").addClass("hiding");
+    $("#gladys").removeClass("hiding");
+    $("#spamela").addClass("hiding");
+    $("#wanda").addClass("hiding");
+    $("#animal").addClass("hiding");
+    $("#beaker").addClass("hiding");
+    $("#scooter").addClass("hiding");
+    $("#kermit").addClass("hiding");
   } else if (gender == 2 && color == "pink") {
-    $("#piggy").show();
-    $("#gladys").hide();
-    $("#spamela").hide();
-    $("#wanda").hide();
-    $("#animal").hide();
-    $("#beaker").hide();
-    $("#scooter").hide();
-    $("#kermit").hide();
+    $("#piggy").removeClass("hiding");
+    $("#gladys").addClass("hiding");
+    $("#spamela").addClass("hiding");
+    $("#wanda").addClass("hiding");
+    $("#animal").addClass("hiding");
+    $("#beaker").addClass("hiding");
+    $("#scooter").addClass("hiding");
+    $("#kermit").addClass("hiding");
   } else if (gender == 2 && color == "red") {
-    $("#piggy").hide();
-    $("#gladys").hide();
-    $("#spamela").show();
-    $("#wanda").hide();
-    $("#animal").hide();
-    $("#beaker").hide();
-    $("#scooter").hide();
-    $("#kermit").hide();
+    $("#piggy").addClass("hiding");
+    $("#gladys").addClass("hiding");
+    $("#spamela").removeClass("hiding");
+    $("#wanda").addClass("hiding");
+    $("#animal").addClass("hiding");
+    $("#beaker").addClass("hiding");
+    $("#scooter").addClass("hiding");
+    $("#kermit").addClass("hiding");
   }
     event.preventDefault();
   });
